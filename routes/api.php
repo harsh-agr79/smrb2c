7ad8e1d9ec7c92d9fa44e92ee7e172c57a2b762f
harsh-agr79 <agrharsh7932@gmail.com>
2024-07-23 21:51:46 +0545

    changes

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+use App\Http\Controllers\AuthController;
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
