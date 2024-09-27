af7b4ec4eabae61bf761c28fb7a19de04973f617
harsh-agr79 <agrharsh7932@gmail.com>
2024-09-27 16:07:12 +0530

    changes

--- resources/views/admin/editproduct.blade.php
+++ resources/views/admin/editproduct.blade.php
@@ -94,8 +94,8 @@
                             class="browser-default inp" placeholder="Price" required>
                     </div>
                     <div class="col m6 s12">
-                        <label> Discount :</label><input name="offer" type="text" value="{{ $prod->offer }}"
-                            class="browser-default inp" placeholder="Offer">
+                        <label> Discount :</label><input name="offer" type="number" value="{{ $prod->offer }}"
+                            class="browser-default inp" placeholder="Discount %">
                     </div>
                     <div class="col s12">
                         <h6>Product Variations</h6>
