e98573a80db5d1bfae00bdf706eda6fe0bc6b906
harsh-agr79 <agrharsh7932@gmail.com>
2024-10-03 19:17:29 +0530

    changes

--- resources/views/emails/forgotpassword.blade.php
+++ resources/views/emails/forgotpassword.blade.php
@@ -6,7 +6,7 @@
 <body>
     <h1>Hello, {{ $name }}</h1>
     <p>You requested to reset your password. Please click the link below to reset it:</p>
-    <a href="{{ $resetUrl }}">Reset Password</a>
+    <a href="{{ $resetUrl }}" style="padding: 10px; background: rgb(0, 140, 255); color: white;">Reset Password</a>
     <p>If you did not request this, please ignore this email.</p>
 </body>
 </html>
