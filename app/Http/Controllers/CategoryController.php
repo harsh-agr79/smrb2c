0efde42d9f2cfd1d75366774d0d3c596ff6c0e5b
harsh-agr79 <agrharsh7932@gmail.com>
2024-10-06 13:47:58 +0530

    changes

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -20,10 +20,22 @@ class CategoryController extends Controller
         $category = Category::get();
         return response()->json($category, 200);
     }
-    public function homeCategory(){
-        $category = Category::whereNotNull('image')->get();
-        return response()->json($category, 200);
+    public function homeCategory() {
+        // Fetch categories where image is not null
+        $categories = Category::whereNotNull('image')->get();
+        
+        // Add the prefix 'categories/' to each image value
+        $categories->map(function ($category) {
+            if ($category->image) {
+                $category->image = 'categories/' . $category->image;
+            }
+            return $category;
+        });
+    
+        // Return the updated categories with the prefixed image path
+        return response()->json($categories, 200);
     }
+    
 
     public function getcategory($id){
         $category = Category::where('id', $id)->first();
