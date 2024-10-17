14d7301d480400ace8e5b95a00b29b3dfa8d1c5e
harsh-agr79 <agrharsh7932@gmail.com>
2024-10-17 19:04:32 +0530

    changes

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -62,7 +62,7 @@ class ProductController extends Controller {
         });
     
         // Return the results as a JSON response
-        return response()->json([$products, $wishlistProductIds]);
+        return response()->json([$products, $user]);
     }
     
 
@@ -148,7 +148,7 @@ class ProductController extends Controller {
             return $product;
         });
     
-        return response()->json([$results, $wishlistProductIds]);
+        return response()->json([$results, $user]);
     }
     
 
